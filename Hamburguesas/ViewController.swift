bf782fef9ba56017f8a5bc49a179d7f3194ea3ed
00aguila <a00aguila@hotmail.com>
2016-07-17 14:05:56 -0500

    Add files via upload

--- Hamburguesas/ViewController.swift
+++ Hamburguesas/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cambio: UILabel!
+    @IBOutlet weak var cambioHam: UILabel!
+    let paises = ColeccionDePaises()
+    let hamburguesas = ColeccionDeHamburguesa()
+    let colores = Colores()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func CambiarPaisHamburguesa() {
+        cambio.text = paises.obtenPais()
+        cambioHam.text = hamburguesas.obtenHamburguesa()
+        let colorAleatorio = colores.regresaColorAleatorio()
+        view.backgroundColor = colorAleatorio
+
+    }
 
 }
 
